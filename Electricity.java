940132bc8c270fd0987518d99d8c5b467ac3addb
ArnavBhokre <arnavbhokre2810@gmail.com>
2026-04-24 15:14:33 +0530

    Add files via upload

---
+++ Electricity.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Electricity{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int units = sc.nextInt();
+        double bill = 50;
+
+        if(units < 100){
+            bill += units * 1.5;
+
+        }
+        else if(units <= 200){
+            bill += 100*1.5+(units-100)*3.5;
+        }
+        else{
+            bill += 100*1.5+100*3.5+(units-200)*5;
+        }
+        bill = bill+50;
+        System.out.println("bill" +bill);
+        if(bill>2000){
+            bill = bill*1.1;
+        }
+        System.out.println("total bill"+bill);
+        sc.close();
+    }
+}
